f605879a55951b8a0ee0e3ed0df5f8a739e0727b
Duncan McIsaac <dbmcisaac1@gmail.com>
2015-10-21 14:15:43 -0400

    got cat images to load

--- app.js
+++ app.js
@@ -3,7 +3,23 @@ $(function() {
 		type: "GET",
 		url: "https://www.reddit.com/r/cats.json?limit=40",
 		success: function(result) {
-			console.log(result);
+			console.log(result.data);
+			var urls = [];
+			for (var i = 0; i < result.data.children.length; i++) {
+				var url = result.data.children[i].data.url;
+				var splitUrl = url.split('/');
+				if (splitUrl[2] === 'imgur.com') {
+					splitUrl[2] = 'i.imgur.com';
+					url = splitUrl.join('/') + '.jpg';
+					// urls.push(url);
+					$('#cats-go-here').append('<img src="'+url+'" />');
+				} else if (splitUrl[2] === 'i.imgur.com') {
+					// urls.push(url);
+					$('#cats-go-here').append('<img src="'+url+'" />');
+				}
+			}
+			// console.log(urls);
+
 		},
 		error: function(jqXHR) {
 			console.log(jqXHR.status);
