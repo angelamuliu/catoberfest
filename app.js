31e8ea50bd6bfe9ef0d8a313798536f2eeb64a8d
Angela Liu <angelamuliu@gmail.com>
2015-10-21 14:20:55 -0400

    cat piano done

--- app.js
+++ app.js
@@ -16,23 +16,32 @@ $(document).ready(function() {
 
     $("#catPiano button").click(function() {
         var note = $(this).attr('id');
+        var audio;
         switch(note) {
             case "do":
+                audio = new Audio('assets/do.wav');
                 break;
             case "re":
+                audio = new Audio('assets/re.wav');
                 break;
             case "me":
+                audio = new Audio('assets/me.wav');
                 break;
             case "fa":
+                audio = new Audio('assets/fa.wav');
                 break;
             case "so":
+                audio = new Audio('assets/so.wav');
                 break;
             case "la":
+                audio = new Audio('assets/la.wav');
                 break;
             case "ti":
+                audio = new Audio('assets/ti.wav');
                 break;
 
         }
+        audio.play();
         return false;
     })
 
